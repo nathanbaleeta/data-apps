25ab1e1a62f12ee7781a53ea5ce63e75fbc3f9c6
nathanbaleeta <nbaleeta@gmail.com>
2025-12-29 22:06:40 +0300

    added requirements.txt for installing dependencies

--- pipelines/scripts/population_by_country_monthly_etl.py
+++
@@ -1,8 +0,0 @@
-# test_script.py
-def my_function(name):
-    print(f"Hello, {name} from a GitHub Action!")
-    return f"Processed: {name}"
-
-if __name__ == "__main__":
-    result = my_function("Bot")
-    print(result)
